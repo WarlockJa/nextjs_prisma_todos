0c222cecacf9f353640f3d03c88a9242a1412bf8
warlockja <warlockja@gmail.com>
2023-11-16 17:25:32 +0300

    css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,50 @@
 import { prisma } from "@/db";
 import Link from "next/link";
 import TodoItem from "./components/TodoItem";
-import { useRouter } from "next/navigation";
 
 function getTodos() {
-    return prisma.todo.findMany();
+  return prisma.todo.findMany();
 }
 
 // server function that saves changes to the DB
 async function toggleTodo(id: string, complete: boolean) {
-    // next js directive to use server side
-    "use server";
+  // next js directive to use server side
+  "use server";
 
-    await prisma.todo.update({ where: { id }, data: { complete } });
+  await prisma.todo.update({ where: { id }, data: { complete } });
 }
 
 // server function that deletes todo in the DB
 async function deleteTodo(id: string) {
-    // next js directive to use server side
-    "use server";
+  // next js directive to use server side
+  "use server";
 
-    await prisma.todo.delete({ where: { id } });
+  await prisma.todo.delete({ where: { id } });
 }
 
 export default async function Home() {
-    const todos = await getTodos();
-    return (
-        <>
-            <header className="flex justify-between items-center mb-4">
-                <h1 className="text-2xl">Todos</h1>
-                <Link
-                    className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
-                    href="/new"
-                >
-                    New
-                </Link>
-            </header>
-            <ul className="pl-4">
-                {todos.map((todo) => (
-                    <TodoItem
-                        key={todo.id}
-                        {...todo}
-                        toggleTodo={toggleTodo}
-                        deleteTodo={deleteTodo}
-                    />
-                ))}
-            </ul>
-        </>
-    );
+  const todos = await getTodos();
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link
+          className="border border-slate-300 text-slate-300 px-2 py-1 rounded transition-colors duration-300 hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          href="/new"
+        >
+          New
+        </Link>
+      </header>
+      <ul className="pl-4 border-2 p-4 rounded-md">
+        {todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            {...todo}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        ))}
+      </ul>
+    </>
+  );
 }
